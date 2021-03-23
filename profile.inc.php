2eb84828783da33ceafb61913e095a7907d23368
Julianna <gagliardijulianna@gmail.com>
2021-03-23 12:24:49 -0600

    Implemented gateway classes for database and user table, and added hamburger menu for mobile screens (width<=500px) as well as styling and js functionality

---
+++ profile.inc.php
@@ -0,0 +1,6 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'stocks');
+define('DBUSER', 'root');
+define('DBPASS', 'LuD!ab1a');
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
---
+++ profile.php
@@ -0,0 +1,38 @@
+<?php
+require_once ("assign_2.classes.inc.php");
+require_once ("profile.inc.php");
+try{
+    $conn = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
+    $userGateway = new UserDB($conn);
+    $userID = 5;
+    $user = $userGateway->getUser($userID);
+}catch (Exception $e) {
+    die( $e->getMessage() );
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="profile.css">
+        <script src="navbar.js"></script>
+    </head>
+    <body>
+<!--    My take on the hamburger nav bar. Based off of https://www.w3schools.com/howto/howto_js_mobile_navbar.asp -->
+        <header class="topnav">
+            <img class="logo" alt="logo">
+            <a class="icon"><i class="fa fa-bars"></i></a>
+            <div id="navlinks" class="hidden">
+                <a href="index.php">Home</a>
+                <a href="about.html">About</a>
+                <a href="list.php">Companies</a>
+                <a class="login" href="portfolio.php">Portfolio</a>
+                <a class="login active" href="profile.php">Profile</a>
+                <a class="login" href="favourites.php">Favourites</a>
+                <a class="login" href="">Log Out</a>
+            </div>
+        </header>
+    </body>
+</html>
