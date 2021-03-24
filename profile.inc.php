54a01126715cd00eff5355356f4c5a6d3d5f7862
Julianna <gagliardijulianna@gmail.com>
2021-03-24 09:55:48 -0600

    minor styling tweeks, changed DB info to use TestUser

--- profile.inc.php
+++ profile.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('DBHOST', 'localhost');
 define('DBNAME', 'stocks');
-define('DBUSER', 'root');
-define('DBPASS', 'LuD!ab1a');
+define('DBUSER', 'TestUser');
+define('DBPASS', 'mypassword');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
