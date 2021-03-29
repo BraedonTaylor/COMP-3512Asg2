1daecc4ad59125d39369ccd064780c63f2ab4056
BraedonTaylor <42498073+BraedonTaylor@users.noreply.github.com>
2021-03-28 22:16:24 -0600

    favorites and home

---
+++ assign_2.classes.inc.php
@@ -0,0 +1,47 @@
+<?php
+class DatabaseHelper {
+    /* Returns a connection object to a database */
+    public static function createConnection( $values=array() ) {
+        $connString = $values[0];
+        $user = $values[1];
+        $password = $values[2];
+        $pdo = new PDO($connString,$user,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+        return $pdo;
+    }
+    /*
+    Runs the specified SQL query using the passed connection and
+    the passed array of parameters (null if none)
+    */
+    public static function runQuery($connection, $sql, $parameters = array()) {
+        // Ensure parameters are in an array
+        if (!is_array($parameters) && $parameters != null) {
+            $parameters = array($parameters);
+        }
+        $statement = null;
+        if (is_array($parameters) && count($parameters) > 0) {
+            // Use a prepared statement if parameters
+            $statement = $connection->prepare($sql);
+            $executedOk = $statement->execute($parameters);
+            if (! $executedOk) throw new PDOException;
+        } else {
+            // Execute a normal query
+            $statement = $connection->query($sql);
+            if (!$statement) throw new PDOException;
+        }
+        return $statement;
+    }
+}
+class UserDB {
+    private static $baseSQL = "SELECT id, firstname, lastname, city, country, email FROM users";
+    public function __construct($connection){
+        $this->pdo = $connection;
+    }
+
+    public function getUser($id){
+        $sql = self::$baseSQL . " WHERE id=?";
+        $statement = DatabaseHelper::runQuery($this->pdo, $sql, array($id));
+        return $statement->fetch();
+    }
+}
---
+++ favorites.php
@@ -0,0 +1,66 @@
+<?php
+require_once ("assign_2.classes.inc.php");
+require_once ("index.inc.php");
+try{
+    $conn = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
+    $userGateway = new UserDB($conn);
+    $userID = 5;
+    $user = $userGateway->getUser($userID);
+}catch (Exception $e) {
+    die( $e->getMessage() );
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Favorites</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="navbar.js"></script>
+    </head>
+    <body>
+<!--    Julianna's take on the hamburger nav bar. Based off of https://www.w3schools.com/howto/howto_js_mobile_navbar.asp -->
+        <header class="topnav">
+            <img class="logo" alt="logo" src="images/logo.png">
+            <a class="icon"><i class="fa fa-bars"></i></a>
+            <div id="navlinks" class="hidden">
+                <a href="index.php">Home</a>
+                <a href="about.html">About</a>
+                <a href="list.php">Companies</a>
+                <a class="login" href="portfolio.php">Portfolio</a>
+                <a class="login" href="profile.php">Profile</a>
+                <a class="login" href="favorites.php">Favorites</a>
+                <a class="login" href="">Log Out</a>
+            </div>
+        </header>
+        <main class="favorites-container">
+            <h2 id="titleText">Favorites</h2>
+            <div id="aapl" class="favoritesListing">
+                <img class="favorites favoritesIcon" alt="Apple" src="images/AAPL.svg">
+                <div class="favorites" id="favoritesSymbol">AAPL</div>
+                <div class="favorites" id="favoritesName">Apple</div>
+                <button class="favorites" id="removeFavorite">Remove</button>
+            </div>
+            <div id="amd" class="favoritesListing">
+                <img class="favorites favoritesIcon" alt="AMD" src="images/AMD.svg">
+                <div class="favorites" id="favoritesSymbol">AMD</div>
+                <div class="favorites" id="favoritesName">Adv. MicroDevices</div>
+                <button class="favorites" id="removeFavorite">Remove</button>
+            </div>
+            <div id="amzn" class="favoritesListing">
+                <img class="favorites favoritesIcon" alt="Amazon" src="images/AMZN.svg">
+                <div class="favorites" id="favoritesSymbol">AMZN</div>
+                <div class="favorites" id="favoritesName">Amazon</div>
+                <button class="favorites" id="removeFavorite">Remove</button>
+            </div>
+            <div id="msft" class="favoritesListing">
+                <img class="favorites favoritesIcon" alt="Microsoft" src="images/MSFT.svg">
+                <div class="favorites" id="favoritesSymbol">MSFT</div>
+                <div class="favorites" id="favoritesName">Microsoft</div>
+                <button class="favorites" id="removeFavorite">Remove</button>
+            </div>
+            <button id="removeAll">Remove All</button>
+        </main>
+    </body>
+</html>
