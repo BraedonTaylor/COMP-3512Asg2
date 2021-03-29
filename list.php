7708bfa7fbbaaae2c74fbc0aad1866da7d004fe8
salimmanji <smanj353@mtroyal.ca>
2021-03-29 00:33:08 -0600

    first push

---
+++ list.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>COMP3512 Assignment 2</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="css/style222.css">
+
+</head>
+
+<body>
+    <main class="container">
+        <header id="header" class="box"><strong>COMP3512 Assignment 2 W2021</strong></header>
+        <div class="box b">
+            <div id="magnify"><img id="magImage"></div>
+            <section>
+                <h2>List Of Companies</h2>
+                Filter: <input type="text" class="search" placeholder="Search for a Company"> <button id="clearButton" class="buttons">Clear Filter</button>
+                <p id="spacer"></p>
+                <section id="companyList"></section>
+
+            </section>
+        </div>
+    </main>
+
+    <script src="js/asg2.js"></script>
+</body>
+
+</html>
