60e2c73aa595fbc72361e9b77e616924ff6f9ee7
Julianna <gagliardijulianna@gmail.com>
2021-04-09 10:48:06 -0600

    added class for company table

--- assign_2.classes.inc.php
+++ assign_2.classes.inc.php
@@ -45,3 +45,22 @@ class UserDB {
         return $statement->fetch(PDO::FETCH_ASSOC);
     }
 }
+class CompanyDB {
+    private static $baseSQL = "SELECT symbol, name, sector FROM companies";
+
+    public function __construct($connection) {
+        $this->pdo = $connection;
+    }
+
+    public function getAll() {
+        $sql = self::$baseSQL;
+        $statement = DatabaseHelper::runQuery($this->pdo, $sql, null);
+        return $statement->fetchAll();
+    }
+
+    public function getAllForSymbol($symbol) {
+        $sql = self::$baseSQL . " WHERE Companies.Symbol=?";
+        $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($symbol));
+        return $statement->fetchAll();
+    }
+}
