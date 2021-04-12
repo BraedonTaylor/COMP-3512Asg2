486fc99ad4b21a0b73ee7062d8199212b7c90428
Julianna <gagliardijulianna@gmail.com>
2021-04-12 15:07:52 -0600

    added link to navbar.css

--- profile.php
+++ profile.php
@@ -26,7 +26,7 @@ try{
         <meta charset="utf-8">
         <title>Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/profile-single.css">
         <link rel="stylesheet" href="css/navbar.css">
         <script src="js/navbar.js"></script>
     </head>
