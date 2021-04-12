0be3e69058b18269b8f1a9dc9e96baa9b6c89904
BraedonTaylor <42498073+BraedonTaylor@users.noreply.github.com>
2021-04-12 16:45:46 -0600

    navbar updated on home and favorites

--- favorites.php
+++ favorites.php
@@ -45,6 +45,7 @@ foreach ($fav as $favorite) {
         <title>Favorites</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/navbar.css">
         <script src="navbar.js"></script>
     </head>
     <body>
