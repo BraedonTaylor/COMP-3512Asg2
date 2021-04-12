ac3324085af56e4905963700282c970636ac5508
Julianna <gagliardijulianna@gmail.com>
2021-04-12 09:23:46 -0600

    minor fixes

--- profile.php
+++ profile.php
@@ -30,7 +30,7 @@ try{
         <script src="js/navbar.js"></script>
     </head>
     <body>
-        <?php buildNav($login, "profile") ?>
+        <?php buildNav($login, "profile"); ?>
         <main class="profile-container">
             <h2 id="profile-head">PROFILE</h2>
             <div class="img">
