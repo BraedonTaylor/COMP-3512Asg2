ac3648c62e1e2f6b4a8f75c93a5efb24f7073577
Julianna <gagliardijulianna@gmail.com>
2021-04-12 09:03:57 -0600

    include file for navbar

--- assign2.navbar.inc.php
+++ assign2.navbar.inc.php
@@ -1,5 +1,5 @@
 <?php
-function buildNav($page, $login = false){
+function buildNav($login, $page = null){
     ?>
     <!-- Nav bar based off of https://www.w3schools.com/howto/howto_js_topnav_responsive.asp -->
     <header class="topnav">
@@ -7,14 +7,14 @@ function buildNav($page, $login = false){
         <a class="icon"><i class="fa fa-bars"></i></a>
         <div id="navlinks" class="hidden">
             <a class="<?php if ($page === "index"){ ?> active <?php } ?>" href="index.php">Home</a>
-            <a href="about.html">About</a>
-            <a href="list.php">Companies</a>
+            <a class="<?php if ($page === "about"){ ?> active <?php } ?>" href="about.php">About</a>
+            <a class="<?php if ($page === "list"){ ?> active <?php } ?>" href="list.php">Companies</a>
             <?php if ($login === true) { ?>
-            <a class="" href="portfolio.php">Portfolio</a>
+            <a class="<?php if ($page === "portfolio"){ ?> active <?php } ?>" href="portfolio.php">Portfolio</a>
             <a class="<?php if ($page === "profile"){ ?> active <?php } ?>" href="profile.php">Profile</a>
-            <a class="" href="favorites.php">Favorites</a>
-            <a class="" href="">Log Out</a>
-            <?php } ?>
+            <a class="<?php if ($page === "favorites"){ ?> active <?php } ?>" href="favorites.php">Favorites</a>
+            <a class="" href="logout.php">Log Out</a>
+            <?php } else { ?> <a class="<?php if ($page === "login"){ ?> active <?php } ?>" href="login.php">Log In</a> <?php } ?>
         </div>
     </header>
 <?php } ?>
