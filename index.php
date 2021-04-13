88ea291138420e6b627a10376f2a2e541a78b8df
Jacob Gill <jacobc.gill@gmail.com>
2021-04-13 14:03:56 -0600

    Fixed index grid stuff

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ try{
         <meta charset="utf-8">
         <title>Home</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="CSS/index.css">
         <link rel="stylesheet" href="css/navbar.css">
         <script src="navbar.js"></script>
         <script src="home.js"></script>
