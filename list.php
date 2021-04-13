0ef99dfe182293398f2bfe48f3f510fa4b4f3c3b
Julianna <gagliardijulianna@gmail.com>
2021-04-13 15:46:19 -0600

    fixed css

--- list.php
+++ list.php
@@ -11,7 +11,6 @@ if (isset($_SESSION["userID"]) && $_SESSION["userID"] != null) {
 <head>
     <meta charset="utf-8" />
     <title>List of Companies</title>
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style222.css">
@@ -26,7 +25,7 @@ if (isset($_SESSION["userID"]) && $_SESSION["userID"] != null) {
             <div id="magnify"><img id="magImage"></div>
             <section>
                 <h1>List Of Companies</h1>
-                Filter: <input type="text" class="search" placeholder="Search for a Company"> <button id="clearButton" class="buttons">Clear Filter</button>
+                Filter: <input type="text" class="search" placeholder="Search for a Company"> <button id="clearButton" class="button">Clear Filter</button>
                 <p id="spacer"></p>
                 <section id="companyList"></section>
             </section>
